cf8a8e8595a4e01fc66fa9e8235801b166bc6111
flight-LM709 <150790738+flight-LM709@users.noreply.github.com>
2023-11-14 17:43:10 +0700

    Add files via upload
    meeting 14

--- src/MathFighterScene.js
+++ src/MathFighterScene.js
@@ -10,6 +10,21 @@ export default class MathFighterScene extends Phaser.Scene {
 		this.player = undefined
 		this.enemy = undefined
 		this.slash = undefined
+		this.startGame = false
+		this.questionText = undefined
+		this.resultText = undefined
+		this.button1 = undefined
+		this.button2 = undefined
+		this.button3 = undefined
+		this.button4 = undefined
+		this.button5 = undefined
+		this.button6 = undefined
+		this.button7 = undefined
+		this.button8 = undefined
+		this.button9 = undefined
+		this.button0 = undefined
+		this.buttonDel = undefined
+		this.buttonOk = undefined
 	}
 	preload() {
 		this.load.image ('background', 'images/bg_layer1.png')
@@ -32,6 +47,7 @@ export default class MathFighterScene extends Phaser.Scene {
             frameWidth: 88,
             frameHeight: 42 
         });
+		this.load.image('start-btn', 'images/start_button.png')
 	}
 
 	create() {
@@ -65,6 +81,14 @@ export default class MathFighterScene extends Phaser.Scene {
 			.setDepth(1)
 			.setCollideWorldBounds(true)
 			this.createAnimation()
+
+			let start_button = this.add.image(this.gameHalfWidth,
+				this.gameHalfHeight + 181, 'start-btn').setInteractive()
+
+			start_button.on('pointerdown', () => {
+					this.gameStart()
+					start_button.destroy()
+				}, this)
 	}
 	update() {
 
@@ -123,6 +147,60 @@ this.anims.create({
 	frameRate: 10
 	})
 	}
-	
+
+	gameStart(){
+		this.startGame = true
+		this.player.anims.play('player-standby', true)
+		this.enemy.anims.play('enemy-standby', true)
+		this.resultText = this.add.text(this.gameHalfWidth,200,
+		// @ts-ignore
+		'0', { fontSize :'32px', fill: '#000'})
+		this.questionText = this.add.text(this.gameHalfWidth,100,
+		// @ts-ignore
+		'0', { fontSize : '32px', fill: '#000' })
+		this.createButtons()
+		}
+
+	createButtons() {
+		const startPosY = this.scale.height - 246
+		const widthDiff = 131
+		const heightDiff = 71.25
+
+		//center buttons
+		// @ts-ignore
+		this.button2 =this.addimage(this.gameHalfWidth,startPosY,
+			'numbers', 1).setInteractive().setData('value', 2)
 			
+			this.button5 = this.add.image(this.gameHalfWidth,this.button2.y
+			+ heightDiff,'numbers',4).setInteractive().setData('value', 5)
+			
+			this.button8 = this.add.image(this.gameHalfWidth,this.button5.y
+			+ heightDiff, 'numbers', 7).setInteractive().setData('value', 8)
+			
+			this.button0 = this.add.image(this.gameHalfWidth, this.button8.y
+			+ heightDiff, 'numbers', 10).setInteractive().setData('value', 0)
+			//left side
+			this.button1 = this.add.image(this.button2.x - widthDiff, startPosY,
+			'numbers', 0).setInteractive().setData('value', 1)
+			this.button4 = this.add.image(this.button5.x - widthDiff,
+			this.button1.y + heightDiff, 'numbers', 3).setInteractive()
+			.setData('value', 4)
+			this.button7 = this.add.image(this.button8.x - widthDiff,
+			this.button4.y + heightDiff, 'numbers', 6).setInteractive()
+			.setData('value', 7)
+			this.buttonDel = this.add.image(this.button0.x - widthDiff,
+			this.button7.y + heightDiff, 'numbers', 9).setInteractive()
+			.setData('value', 'del')
+			this.button3 = this.add.image(this.button2.x + widthDiff, startPosY,
+				'numbers', 2).setInteractive().setData('value', 3)
+				this.button6 = this.add.image(this.button5.x + widthDiff,
+				this.button3.y + heightDiff, 'numbers', 5).setInteractive()
+				.setData('value', 6)
+				this.button9 = this.add.image(this.button8.x + widthDiff,
+				this.button6.y + heightDiff, 'numbers', 8).setInteractive()
+				.setData('value', 9)
+				this.buttonOk = this.add.image(this.button0.x + widthDiff,
+				this.button9.y + heightDiff, 'numbers', 11).setInteractive()
+				.setData('value', 'ok')
+	}
 }
